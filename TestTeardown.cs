e5d9be04ed44fe8c207742b48240f97892a65664
Brian Kitchener <kitchener.brian@gmail.com>
2014-04-28 10:57:21 -0600

    adding testteardown attribute

---
+++ TestTeardown.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using Gallio.Common.Collections;
+using Gallio.Common.Reflection;
+using Gallio.Framework;
+using Gallio.Framework.Pattern;
+using Gallio.Model;
+using MbUnit.Framework;
+
+namespace ProtoTest.Nightshade
+{
+    [AttributeUsage(PatternAttributeTargets.Test, AllowMultiple = true, Inherited = true)]
+    public class TestTeardownAttribute : TestDecoratorPatternAttribute
+    {
+
+        public string methodName;
+
+        public TestTeardownAttribute(string methodName)
+        {
+            this.methodName = methodName;
+
+
+        }
+
+        /// <inheritdoc />
+        protected override void DecorateTest(IPatternScope scope, ICodeElementInfo codeElement)
+        {
+            //scope.TestBuilder.TestInstanceActions.AfterTestInstanceChain.After(delegate(
+            //    PatternTestInstanceState instanceState)
+            //    {
+
+            //        ITypeInfo typeInfo;
+            //        typeInfo = ReflectionUtils.GetType(codeElement);
+            //        IMethodInfo method = typeInfo.GetMethod(this.methodName, BindingFlags.Instance | BindingFlags.Public);
+            //        instanceState.InvokeFixtureMethod(method,
+            //                                          (IEnumerable<KeyValuePair<ISlotInfo, object>>)
+            //                                          EmptyArray<KeyValuePair<ISlotInfo, object>>.Instance);
+
+            //    });
+
+
+            scope.TestBuilder.TestInstanceActions.TearDownTestInstanceChain.Before(delegate(PatternTestInstanceState instanceState)
+            {
+
+                ITypeInfo typeInfo;
+                typeInfo = ReflectionUtils.GetType(codeElement);
+                IMethodInfo method = typeInfo.GetMethod(this.methodName, BindingFlags.Instance | BindingFlags.Public);
+                instanceState.InvokeFixtureMethod(method,
+                                                  (IEnumerable<KeyValuePair<ISlotInfo, object>>)
+                                                  EmptyArray<KeyValuePair<ISlotInfo, object>>.Instance);
+
+            });
+        }
+    }
+}
